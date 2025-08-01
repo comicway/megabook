ba40d040abdc91622cf93a47d3114fbf78c03f34
comicway <mises.xiaomi@gmail.com>
2025-08-01 07:41:46 -0400

    modificacion de componente daily streak y ajuste css

--- src/Components/DailyStreak/DailyStreak.jsx
+++ src/Components/DailyStreak/DailyStreak.jsx
@@ -1,30 +1,32 @@
 const DailyStreak = () => {
-  return (
-    <>
-        <div className="container mx-auto">
-            <div className="grid gap-3">
-                <div className="grid-cols-1">
-                    <div>
-                        <p>3</p>
-                        <p>Dias</p>
-                    </div>
-                    <div>
-                        <ul>
-                            <li><p>D</p><span>check</span></li>
-                            <li><p>L</p><span>check</span></li>
-                            <li><p>M</p><span>check</span></li>
-                            <li><p>M</p><span>check</span></li>
-                            <li><p>J</p><span>check</span></li>
-                            <li><p>V</p><span>check</span></li>
-                            <li><p>S</p><span>check</span></li>
-                        </ul>
-                        <p>SEMANAS</p>
+    return (
+        <>
+            <div className="container mx-auto">
+                <div className="grid gap-3">
+                    <div className="grid-cols-1">
+                        <div className="cardStreak">
+                            <div style={{ width: "20%" }}>
+                                <p>3</p>
+                                <p>Dias</p>
+                            </div>
+                            <div style={{ width: "80%" }}>
+                                <ul>
+                                    <li><p>D</p><span>check</span></li>
+                                    <li><p>L</p><span>check</span></li>
+                                    <li><p>M</p><span>check</span></li>
+                                    <li><p>M</p><span>check</span></li>
+                                    <li><p>J</p><span>check</span></li>
+                                    <li><p>V</p><span>check</span></li>
+                                    <li><p>S</p><span>check</span></li>
+                                </ul>
+                                <p>SEMANAS</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-             </div>
-        </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default DailyStreak;
