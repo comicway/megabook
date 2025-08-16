fbfe215281d7b51a21d7fe274ff9dcafac7c6db8
comicway <mises.xiaomi@gmail.com>
2025-08-16 15:10:37 -0400

    creacion de varios componentes v2

---
+++ src/Components/ResgiterBook/RegisterBook.jsx
@@ -0,0 +1,13 @@
+const RegisterBook = () => {
+    return (
+        <>
+            <div className="container mx-auto px-2 mt-[20px]">
+                    <div className="grid grid-cols-1">
+                        <form action=""></form>
+                    </div>
+            </div>
+        </>
+    );
+}
+
+export default RegisterBook;
