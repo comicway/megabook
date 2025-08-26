015d71e1c29bc065c23320568f3f9087902baa1e
comicway <mises.xiaomi@gmail.com>
2025-08-26 06:28:37 -0400

    empezando creando formulario con formit

--- src/Components/ResgiterBook/RegisterBook.jsx
+++ src/Components/ResgiterBook/RegisterBook.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-const validateBook = value => {
-    let error;
+const validate = (values) => {
+    const errors = {};
     if (!value.book) {
-        error = 'El nombre del libro es requerido';
-    } 
-    return error;
-};
-
-const validateAutor = value => {
-    let error;
-    if (!value.autor) {
-        error = 'El nombre del autor es requerido';
-    } else if (!/[a-zA-Z\s]+$/i.test(value,autor)) {
-        error = 'Solo se permiten letras';
+        error.book = 'El nombre del libro es requerido';
     }
-    return error;
-};
-
-const validateISBN = value => {
-    let error;
+    if (!value.autor) {
+        error.autor = 'El nombre del autor es requerido';
+    } else if (!/[a-zA-Z\s]+$/i.test(value.autor)) {
+        error.autor = 'Solo se permiten letras';
+    }
     if (!value.ISBN) {
-        error = 'El codigo ISBN es requerido';
+        error.ISBN = 'El codigo ISBN es requerido';
     } else if (!/^[0-9]+$/i.test(value.ISBN)) {
-        error = 'Solo se permiten numeros';
+        erro.ISBN = 'Solo se permiten numeros';
     }
     return error;
 };
@@ -35,15 +25,44 @@ const RegisterBook = () => {
             <div className="container mx-auto px-2 mt-[20px]">
                     <div className="grid grid-cols-1">
                         <Formik
-                            initialValues={{ book: '', autor: '', ISBN: '' }}
-                            onSubmit={(values, { setSubmitting, resetForm }) => {
-                                setTimeout(() => {
-                                    alert(JSON.stringify(values, null, 2));
-                                    setSubmitting(false);
-                                    resetForm();
-                                }, 400);
+                            initialValues={{ 
+                                book: '', 
+                                autor: '', 
+                                ISBN: '' 
                             }}
+                            validate={validate}
                         >
+                        {({ issSubmitting}) => (
+                            <Form className='container mx-auto'>
+                                <div>
+                                    <Field name="book" type="text" placeholder="Titulo del libro*"></Field>
+                                </div>
+                                <div className="text-[#BF3A0A] flex justify-center">
+                                    <ErrorMessage name="book"/>
+                                </div>
+                                <div>
+                                    <Field name="autor" type="text" placeholder="Nombre del autor*"></Field>
+                                </div>
+                                <div className="text-[#BF3A0A] flex justify-center">
+                                    <ErrorMessage name="autor"/>
+                                </div>
+                                <div>
+                                    <Field name="ISBN" type="text" placeholder="ISBN del libro*"></Field>
+                                </div>
+                                <div className="text-[#BF3A0A] flex justify-center">
+                                    <ErrorMessage name="ISBN"/>
+                                </div>
+                                <div className="flex justify-center">
+                            <button
+                                type="submit"
+                                className=""
+                            >
+                                Enviar
+                            </button>
+                        </div>
+                            </Form>
+
+                        )} 
                         </Formik>       
                     </div>
             </div>
