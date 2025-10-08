0bb8e786d39892d701025e9a14de6dc921ca3053
comicway <mises.xiaomi@gmail.com>
2025-10-08 09:51:18 -0300

    prueba exitosa api Google Book

--- src/Components/BookLog/BookLog.jsx
+++ src/Components/BookLog/BookLog.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const BookLog = () => {
 
-    const [book, setBook] = useState(null);
+    const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -11,7 +11,7 @@ const BookLog = () => {
         const fetchBookData = async () => {
             
             const apiKey = 'AIzaSyBzpG3HDLwYjHSYiEPJxgKVTyOizFL33cY';
-            const query = "Harry Potter y la piedra filosofal";
+            const query = "yo robot";
             const encodedQuery = encodeURIComponent(query);
 
             const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodedQuery}&key=${apiKey}`;
@@ -26,10 +26,12 @@ const BookLog = () => {
                 const data = await response.json();
 
                 if (data.items && data.items.length > 0) {
-                    setBook(data.items[0].volumeInfo);
-                }else {
+                    setBooks(data.items);
+                } else {
                     throw new Error('No se encontraron libros con este titulo.');
                 }
+                return data;
+
             } catch (err) {
                 setError(err.message);
             } finally {
@@ -49,7 +51,7 @@ const BookLog = () => {
     return <div>Error: {error}</div>;
   }
 
-  if (!book) {
+  if (!books) {
     return null;
   }
 
@@ -68,24 +70,13 @@ const BookLog = () => {
             <div className="container mx-auto px-2 mt-[8px]">
                 <div className="">
                     <div className="grid gap-3 grid-cols-3 lg:grid-cols-4">
-                        <div className="book-card">
-                            <img src="https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL._SX329_BO1,204,203,200_.jpg" alt="Portada del libro" className=""/>
-                        </div>
-                        <div className="book-card">
-                            <img src="https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL._SX329_BO1,204,203,200_.jpg" alt="Portada del libro" className=""/>
-                        </div>
-                        <div className="book-card">
-                            <img src="https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL._SX329_BO1,204,203,200_.jpg" alt="Portada del libro" className=""/>
-                        </div>
-                        <div className="book-card">
-                            <img src="https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL._SX329_BO1,204,203,200_.jpg" alt="Portada del libro" className=""/>
-                        </div>
-                        <div className="book-card">
-                            <img src="https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL._SX329_BO1,204,203,200_.jpg" alt="Portada del libro" className=""/>
-                        </div>
-                        <div className="book-card">
-                            <img src="https://images-na.ssl-images-amazon.com/images/I/51N-u8AsmdL._SX329_BO1,204,203,200_.jpg" alt="Portada del libro" className=""/>
-                        </div>
+                        {books.map((book) => (
+                            <div className="book-card" key={book.id}>
+                                {book.volumeInfo.imageLinks?.thumbnail && (
+                                    <img src={book.volumeInfo.imageLinks.thumbnail} alt={`Portada de ${book.volumeInfo.title}`}/>
+                                 )}
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
